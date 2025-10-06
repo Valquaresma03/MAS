ab442c6c3626184953259e9f559df3bacc76c27f
2030-Y <2030170@proton.me>
2025-10-06 16:50:06 +0200

    Add files via upload

---
+++ assignment1/test2.py
@@ -0,0 +1,23 @@
+
+import irsim
+from custom_behavior_methods_2 import metrics_p
+
+env = irsim.make("test2.yaml")
+env.load_behavior("custom_behavior_methods_2")
+
+for _ in range(500):
+    env.step()
+    env.render(0.03)
+    if env.done():
+        break
+
+metrics_p()
+env.end(3)
+
+
+
+
+
+
+
+
